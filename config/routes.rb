47b2af3a4c41625c4c764262a1d9b7c83f202fef
AYAITABASHI <119820092+AYA-ITABASHI@users.noreply.github.com>
2023-02-15 13:15:14 +0000

    [Add]新規登録画面のviewだけ

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
 
+  namespace :public do
+    get 'cart_items/index'
+  end
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
    sessions: "admin/sessions"
   }
 
-  devise_for :customer, skip: [:passwords], controllers: {
+  devise_for :customers, skip: [:passwords], controllers: {
    registrations: "public/registrations",
    sessions: 'public/sessions'
   }
 
   root to:'public/homes#top'
   get 'public/homes/about' => 'public/homes#about', as:'about'
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
