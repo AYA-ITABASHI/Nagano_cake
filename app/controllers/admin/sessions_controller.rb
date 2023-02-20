37ce111c213947cfa4ae645db17ea2942e65b74d
AYAITABASHI <119820092+AYA-ITABASHI@users.noreply.github.com>
2023-02-20 23:06:02 +0000

    [Add]商品登録画面

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+
+   def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:email])
+   end
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,10 @@ Rails.application.routes.draw do
     get 'genres/edit'
   end
   namespace :admin do
-    get 'items/index'
+    get 'items'=>'items#index'
     get 'items/new'
-    get 'items/show'
-    get 'items/edit'
+    get 'items/:id' =>'items#show'
+    get 'items/:id/edit' => 'items#edit'
   end
   namespace :admin do
     root to:'homes#top'
